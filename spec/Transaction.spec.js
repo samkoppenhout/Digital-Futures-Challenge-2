3486f53bce5df390f12c83f53207df991fd9e83a
Sam K <samkoppenhout@live.com>
2024-05-02 13:39:36 +0100

    updating code to pass new test

---
+++ spec/Transaction.spec.js
@@ -0,0 +1,67 @@
+import Transaction from "../src/Transaction.js"
+
+let testDate, testType, testAmount, transaction
+
+beforeEach(() => {
+    testDate = undefined;
+    testType = undefined;
+    testAmount = undefined;
+    transaction = undefined;
+})
+
+describe("Transaction Class Tests:", () => { 
+    it("should create a transaction object with the correct date value when the constructor is called", () => {
+        // Arrange
+        testDate = "22/02/04"
+        testType = "deposit"
+        testAmount = 1
+        // Act
+        transaction = new Transaction(testDate, testType, testAmount)
+        // Assess
+        expect(transaction.getDate()).toBe(testDate)
+    })
+
+    it("should create a transaction object with the correct type value when the constructor is called", () => {
+        // Arrange
+        testDate = "22/02/04"
+        testType = "deposit"
+        testAmount = 1
+        // Act
+        transaction = new Transaction(testDate, testType, testAmount)
+        // Assess
+        expect(transaction.getType()).toBe(testType)
+    })
+
+    it("should create a transaction object with the correct amount value when the constructor is called", () => {
+        // Arrange
+        testDate = "22/02/04"
+        testType = "deposit"
+        testAmount = 1
+        // Act
+        transaction = new Transaction(testDate, testType, testAmount)
+        // Assess
+        expect(transaction.getAmount()).toBe(testAmount)
+    })
+
+    it("should report not valid if the amount is negative", () => {
+        // Arrange
+        testDate = "22/02/04"
+        testType = "deposit"
+        testAmount = -1
+        // Act
+        transaction = new Transaction(testDate, testType, testAmount)
+        // Assess
+        expect(transaction.isValid()).toBe(false)
+    })
+
+    it("should report not valid if the amount is not a number", () => {
+        // Arrange
+        testDate = "22/02/04"
+        testType = "deposit"
+        testAmount = "string"
+        // Act
+        transaction = new Transaction(testDate, testType, testAmount)
+        // Assess
+        expect(transaction.isValid()).toBe(false)
+    })
+})
