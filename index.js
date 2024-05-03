ded394440acd232851af2e43245e26d59da83d37
Sam K <samkoppenhout@live.com>
2024-05-03 17:59:02 +0100

    updating index demonstration

--- index.js
+++ index.js
@@ -2,27 +2,28 @@ import Account from "./src/Account.js";
 import Transaction from './src/Transaction.js';
 import Printer from "./src/StatementPrinter.js"
 
-let account = new Account(1000);
 let transaction;
+let account = new Account(0);
 
-transaction = new Transaction("01/01/2022", "deposit", 500);
-account.tryAddTransaction(transaction);
-
-transaction = new Transaction("05/01/2022", "deposit", 25);
+transaction = Transaction.createTransaction("01/01/2022", "deposit", 500);
 account.tryAddTransaction(transaction);
 
-transaction = new Transaction("10/01/2022", null, 5);
+transaction = Transaction.createTransaction("05/01/2022", "deposit", 25);
 account.tryAddTransaction(transaction);
 
-transaction = new Transaction("15/01/2022", "withdrawal", 1000);
+transaction = Transaction.createTransaction("10/01/2022", null, 5);
 account.tryAddTransaction(transaction);
 
-transaction = new Transaction("20/01/2022", "deposit", 20);
+transaction = Transaction.createTransaction("15/01/2022", "withdrawal", 1000);
+account.tryAddTransaction(transaction);
+
+transaction = Transaction.createTransaction("20/01/2022", "deposit", 20);
 account.tryAddTransaction(transaction);
 account.tryAddTransaction(transaction);
 account.tryAddTransaction(transaction);
 
-transaction = new Transaction("22/01/2022", "withdrawal", 50);
+transaction = Transaction.createTransaction("22/01/2022", "withdrawal", 50);
 account.tryAddTransaction(transaction);
 
 Printer.print(account.getTransactionHistory());
+
